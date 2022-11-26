28182fc8ec99f67e9f0e79c2acbf03a4981e6fd7
kenya6565 <ken.australia0605@gmail.com>
2022-11-26 23:45:59 +0900

    get amount state through useSelector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const { amount } = useSelector((store) => store.cart);
   return (
     <div className="nav-center">
       <h3>Redux Shopping</h3>
       <div className="nav-container">
         ロゴ
         <div className="amount-container">
-          <p className="total-amount">0</p>
+          <p className="total-amount">{amount}</p>
         </div>
       </div>
     </div>
