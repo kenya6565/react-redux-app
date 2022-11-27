c11388cfeacda1f39f47b322ebeb2bac9d5491fa
kenya6565 <ken.australia0605@gmail.com>
2022-11-27 17:16:53 +0900

    display Cart information

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { MinusIcon, PlusIcon } from "../HeroIcons";
 
-const CartItem = () => {
+const CartItem = (props) => {
+  const { img, id, title, price, amount } = props;
   return (
-    <div>
-      CartItem
-    </div>
-  )
-}
+    <article className="cart-item">
+      <img src={img} alt="" />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">{price}円</h4>
+        <button className="remove-btn">削除</button>
+        <button className="amount-btn">
+          <PlusIcon />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <MinusIcon />
+        </button>
+      </div>
+    </article>
+  );
+};
 
-export default CartItem
+export default CartItem;
