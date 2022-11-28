211a95026579009f5844fa0c17325315be98f697
kenya6565 <ken.australia0605@gmail.com>
2022-11-28 23:48:31 +0900

    add useEffect towards calculateTotals

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import { useSelector } from "react-redux";
 import "./App.css";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/CartSlice";
+
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  // cartItemsに変更があった際に計算ロジックを走るようにしたい
+  // useEffectなしだと全てのcartItemsに変更を及ぼすeventに対してcalculateTotals()
+  // をつけないといけない
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
