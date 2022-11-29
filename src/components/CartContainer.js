030d1f897665b1a176ff9a1f09a4ed0720ce84e2
kenya6565 <ken.australia0605@gmail.com>
2022-11-30 00:10:42 +0900

    create ModalSlice.js

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import CartItem from "./CartItem";
 import { clearCart } from "../features/cart/CartSlice";
-　
 const CartContainer = () => {
   const { amount, cartItems, total } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
